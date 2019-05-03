d306890c34f182f0c43ea7adf146bf8dbe04e746
kishoret99 <saikishoret99@gmail.com>
2019-05-03 10:55:04 +0530

    added admin tools folder

---
+++ admin-tools/x.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const lyrics = fs.readFileSync('./xz.txt', 'utf-8');
+let singleLineLyric = '';
+lyrics.split('\n').forEach(lyric => {
+    if(!lyric) return;
+    singleLineLyric += lyric + '\\n';
+});
+
+console.log(singleLineLyric);
