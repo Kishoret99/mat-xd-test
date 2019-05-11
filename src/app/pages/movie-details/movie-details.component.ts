2961916a6528528adb5700d46c25cf4642d45db3
kishoret99 <saikishoret99@gmail.com>
2019-05-11 19:12:26 +0530

    movie details fetching from afs

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore'
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface Movie {
+  artists: Array<string>,
+  directors: string,
+  languageCode: string,
+  musicDirector: string,
+  releaseDate: Date,
+  title: string,
+  imageUrl: string,
+  songs: AngularFirestoreCollection
+}
+
+export interface Song {
+  duration: string;
+  languageCode: string;
+  lyricist: string;
+  lyrics: string;
+  singers: Array<string>;
+  title: string;
+}
+
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +31,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() movieName;
+  public movie: Observable<Movie>;
+  public songs: Observable<Array<Song>>;
+  private movieDocument: AngularFirestoreDocument<Movie>;
+  private moviesCollection: AngularFirestoreCollection<Movie>;
+  private songsCollection: AngularFirestoreCollection<Song>;
+
+  constructor(
+    private afs: AngularFirestore,
+    private router: Router,
+    private activateRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.activateRoute.params.subscribe(param => {
+      if(param.movieName) {
+        this.movieDocument = this.afs.doc(`movies/${param.movieName}`);
+        this.movie = this.movieDocument.valueChanges();
+        this.songsCollection = this.movieDocument.collection('songs');
+        this.songs = this.songsCollection.valueChanges();
+
+        //DEBUG
+        this.movie.subscribe(movie => {
+          console.log('> movie', movie);
+        });
+
+        this.songs.subscribe(songs => {
+          console.log('> songs', songs);
+        })
+      }
+    })
   }
 
 }
