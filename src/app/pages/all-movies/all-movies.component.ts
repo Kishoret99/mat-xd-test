ffa467747f38a68cf7066bf3cb05bd9cff9b1133
kishoret99 <saikishoret99@gmail.com>
2019-05-14 13:14:03 +0530

    all movies navigation

--- src/app/pages/all-movies/all-movies.component.ts
+++ src/app/pages/all-movies/all-movies.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
 import { Movie } from '../../entities';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+export interface MovieWithId extends Movie {
+  id: string;
+}
 
 @Component({
   selector: 'app-all-movies',
@@ -9,19 +15,30 @@ import { Movie } from '../../entities';
 })
 export class AllMoviesComponent implements OnInit {
 
-  public movies: Array<Movie>
+  public movies: Array<MovieWithId>
   private moviesCollection: AngularFirestoreCollection<Movie>;
 
   constructor(
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private router: Router
   ) { 
 
   }
 
   ngOnInit() {
     this.moviesCollection = this.afs.collection('movies');
-    this.moviesCollection.valueChanges().subscribe(movies => {
+    this.moviesCollection.snapshotChanges().pipe(map(actions => {
+      return actions.map(action => {
+        const data = action.payload.doc.data() as Movie;
+        const id = action.payload.doc.id;
+        return {id, ...data}
+      })
+    })).subscribe(movies => {
       this.movies = movies;
     })
+  }
+
+  naviagteTo(movie) {
+    this.router.navigateByUrl(`/movies/${movie.id}`);
   }
 }
