c2afb86b5e7b79af39612679047dd3248e36b52f
kishoret99 <saikishoret99@gmail.com>
2019-06-08 17:02:55 +0530

    0.20.0 release commit

--- static.paths.ts
+++ static.paths.ts
@@ -2,5 +2,14 @@ export const ROUTES = [
   '/',
   '/home',
   '/movies',
-  '/movies/dGqhBmL5FAbwaoVHCtZf'
+  '/movies/F28rPVpmuEvpcsCbh4Q3',
+  '/movies/SLR1WhopFoNe0ouIIyhN',
+  '/movies/ZGeNwBsxcrEp9qMNU4wn',
+  '/movies/agnyaathavaasi-2018',
+  '/movies/dGqhBmL5FAbwaoVHCtZf',
+  '/movies/ebNv170UTRwxkQUBGjvw',
+  '/movies/f5rHQPW7HHY9KmGhsFWW',
+  '/movies/geetha-govindam-2018',
+  '/movies/lOfiQUYxTFCtTgEewoJy',
+  '/movies/mahanati-2018'
 ];
