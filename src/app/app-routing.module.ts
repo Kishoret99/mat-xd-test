231099101fc4b63a05dc825cd4afcb6cd22f7092
kishoret99 <saikishoret99@gmail.com>
2019-06-28 15:09:43 +0530

    saving work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AllMoviesComponent } from './pages/all-movies/all-movies.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent },
-  {path: 'movies', component: AllMoviesComponent },
-  // {path: 'movies/:movieName',loadChildren: () => import('./pages/movie-details/movie-details.module').then(mod => mod.MovieDetailsModule)},
-  // {path: 'movies/:movieName/lyrics/:songName',loadChildren: () => import('./pages/lyric-details/lyric-details.module').then(mod => mod.LyricDetailsModule)},
-  // {path: 'search', loadChildren: () => import('./pages/search/search.module').then(mod => mod.SearchModule)}
+  {path: 'movies/:movieName',loadChildren: () => import('./pages/movie-details/movie-details.module').then(mod => mod.MovieDetailsModule)},
+  {path: 'movies/:movieName/lyrics/:songName',loadChildren: () => import('./pages/lyric-details/lyric-details.module').then(mod => mod.LyricDetailsModule)},
+  {path: 'search', loadChildren: () => import('./pages/search/search.module').then(mod => mod.SearchModule)},
 ];
 
 @NgModule({
